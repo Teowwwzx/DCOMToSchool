0fa993e6b3f20d105d4c6146e5dce09ed8374551
Teowwwzx-alitec <zhenxiang.teow@alitec.asia>
2025-08-27 23:20:50 +0800

    Final version for presentation

--- src/com/wipro/payroll/client/Client.java
+++ src/com/wipro/payroll/client/Client.java
@@ -17,10 +17,8 @@ import java.util.stream.Collectors;
 public class Client {
     private static final Logger log = LoggerFactory.getLogger(Client.class);
     private static String[] SERVER_IPS = {
+            "47.129.38.215",
             "localhost",
-//            "47.129.38.215",
-//            "192.168.100.200",
-//            "192.168.100.35",
     };
     private static final int RMI_PORT = 1099;
     private static final String SERVICE_NAME = "PayrollService";
