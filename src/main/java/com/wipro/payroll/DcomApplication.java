6816f4c24ca22e18782adc3589b6695263a13310
Teowwwzx <teowwwzx@gmail.com>
2026-02-01 03:33:07 +0000

    Chore: Initialize modernization to Spring Boot 3 structure

---
+++ src/main/java/com/wipro/payroll/DcomApplication.java
@@ -0,0 +1,11 @@
+package com.wipro.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcomApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DcomApplication.class, args);
+    }
+}
